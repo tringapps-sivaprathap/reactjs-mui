efb3ddc427f3167e0a8af6e01cd9603df7286e09
Sivaprathap S <102897756+tringapps-sivaprathap@users.noreply.github.com>
2022-05-10 17:41:53 +0530

    files updated

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import DisplayCards from './components/DisplayCards';
 function App() {
   const [data, setData] = useState([]);
   const [index, setIndex] = useState(0);
-  const [name, setName] = useState('');
-  const [comments, setComments] = useState('');
+  const [proName, setProName] = useState('');
+  const [proPrice, setProPrice] = useState('');
   const [productFlag, setProductFlag] = useState(false);
 
   const {register, handleSubmit, formState: { errors }} = useForm();
@@ -20,15 +20,15 @@ function App() {
   const onSubmit = (formData) => {
     let user = {
       id: index,
-      name: formData.productName,
-      comments: formData.productPrice
+      proName: formData.productName,
+      proPrice: formData.productPrice
     }
     
     setData([...data, user]);
     setIndex(index + 1);
 
-    setName('');
-    setComments('');
+    setProName('');
+    setProPrice('');
     setProductFlag(true);
   }
 
@@ -41,14 +41,36 @@ function App() {
       <Paper sx={{padding: '2rem', width: '30vw'}} elevation={2}>
         <form autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
           <Stack direction='column' spacing={2}>
-            <TextField error={Boolean(errors?.productName)} helperText={errors?.productName && errors.productName.message} {...register("productName", { required: "Required" })} type="text" label="Product Name" value={name} variant='outlined' color='primary' required autoFocus onChange={(e) => {setName(e.target.value)}}  />
-            <TextField error={Boolean(errors?.productPrice)} helperText={errors?.productName ? errors.productName.message : ''} {...register("productPrice", { required: "Required" })} type="text" label="Product Price" value={comments} variant='outlined' color='primary' required onChange={(e) => {setComments(e.target.value)}} />
+            <TextField
+              type='text' label="Product Name" variant='outlined' required autoFocus
+
+              error={Boolean(errors?.productName)}
+              helperText={errors?.productName && errors.productName.message}
+
+              {...register("productName", { required: "Required" })}
+              value={proName}
+
+              onChange={(e) => {setProName(e.target.value)}}
+            />
+
+            <TextField 
+              type='number' label="Product Price" variant='outlined' required
+
+              error={Boolean(errors?.productPrice)} 
+              helperText={errors?.productName ? errors.productName.message : ''} 
+
+              {...register("productPrice", { required: "Product price required!" })} 
+              value={proPrice}
+
+              onChange={(e) => {setProPrice(e.target.value)}}
+            />
+
             <Button type='submit' variant='contained' size='small'>Submit</Button>
           </Stack>
         </form>
       </Paper>
       
-      <DisplayCards data={data} setData={setData} setName={setName} setComments={setComments} productFlag={productFlag}/>
+      <DisplayCards data={data} setData={setData} setProName={setProName} setProPrice={setProPrice} productFlag={productFlag}/>
     </>
   );
 }
--- src/components/DisplayCards.js
+++ src/components/DisplayCards.js
@@ -9,10 +9,13 @@ import {
 import MoreVertIcon  from '@mui/icons-material/MoreVert';
 import ProductImage from '../assets/product-picture.jpg';
 
-const DisplayCards = ({ data, setData, setName, setComments, productFlag }) => {
+const DisplayCards = ({ data, setData, setProName, setProPrice, productFlag }) => {
   const [clickIndex, setClickIndex] = useState(null);
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
+
+  const optionsClick = (event, index) => {setAnchorEl(event.currentTarget); setClickIndex(index);};
+  
   // for alert
   const [openPop, setOpenPop] = useState(false);
 
@@ -21,8 +24,6 @@ const DisplayCards = ({ data, setData, setName, setComments, productFlag }) => {
   const cancelClosePop = () => {setOpenPop(false)};
 
   const yesClosePop = () => {deleteCard(); setOpenPop(false);}
-
-  const handleClick = (event, index) => {setAnchorEl(event.currentTarget); setClickIndex(index);};
 
   const handleClose = () => {setAnchorEl(null)};
   
@@ -37,8 +38,8 @@ const DisplayCards = ({ data, setData, setName, setComments, productFlag }) => {
   const editCard = () => {
     data.forEach(user => {
         if(user.id === clickIndex) {
-            setName(user.name);
-            setComments(user.comments);
+            setProName(user.proName);
+            setProPrice(user.proPrice);
         }
     });
     setData(data.filter((user) => {
@@ -49,44 +50,44 @@ const DisplayCards = ({ data, setData, setName, setComments, productFlag }) => {
 }
 
   return (
-      <>
+    <>
       <Container sx={{ padding: '1rem'}}>
-      {productFlag && <Typography variant="h4" component="h4" className='product-section-title'>Products List</Typography>}
-      <Grid container spacing={3} sx={{ margin: '2rem 0'}}>
-        {data.map((user) => {
+        {productFlag && <Typography variant="h4" component="h4" className='product-section-title'>Products List</Typography>}
+        
+        <Grid container spacing={3} sx={{ margin: '2rem 0'}}>
+          {data.map((user) => {
             return (
               <Grid key={user.id} item md={3}>
                 <Card elevation={2}>
                     <CardHeader
-                        action={
-                            <IconButton id="options-button" onClick={(event) => {handleClick(event, user.id)}}
-                                aria-controls={open ? 'options-menu' : undefined}
-                                aria-haspopup="true"
-                                aria-expanded={open ? 'true' : undefined}
-                            >
-                              <MoreVertIcon />
-                            </IconButton>
-                        }
-                        title={user.name}
+                      action={
+                          <IconButton id="options-button" onClick={(event) => {optionsClick(event, user.id)}}
+                            aria-controls={open ? 'options-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                          >
+                            <MoreVertIcon />
+                          </IconButton>
+                      }
+                      title={user.proName}
                     />
+
                     <CardMedia
-                        component="img"
-                        height="200vh"
-                        image={ProductImage}
-                        alt="Product Picture"
+                      component="img"
+                      height="200vh"
+                      image={ProductImage}
+                      alt="Product Picture"
                     />
+
                     <CardContent>
-                        <Typography variant='body'>
-                            {user.name + ' price is $' + user.comments}
-                        </Typography>
+                      <Typography variant='body'>{user.proName + ' price is $' + user.proPrice}</Typography>
                     </CardContent>
                 </Card>
-                </Grid>
+              </Grid>
             );
-        })}
+          })}
         </Grid>
       </Container>
-
 
       <Menu id="options-menu" anchorEl={anchorEl} open={open} onClose={handleClose} MenuListProps={{'aria-labelledby': 'options-button'}}>
         <MenuItem onClick={editCard}>Edit</MenuItem>
@@ -95,12 +96,13 @@ const DisplayCards = ({ data, setData, setName, setComments, productFlag }) => {
 
       <Dialog open={openPop} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
         <DialogTitle id="alert-dialog-title">{"Are you sure want to delete?"}</DialogTitle>
+
         <DialogActions>
           <Button onClick={cancelClosePop} autoFocus>Cancel</Button>
-          <Button variant='contained' onClick={yesClosePop}>Yes</Button>
+          <Button onClick={yesClosePop} variant='contained'>Yes</Button>
         </DialogActions>
       </Dialog>
-      </>
+    </>
   );
 }
 
